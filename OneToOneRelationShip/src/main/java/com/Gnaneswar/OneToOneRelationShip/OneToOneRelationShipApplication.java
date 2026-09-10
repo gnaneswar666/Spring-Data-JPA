83749c36f66140f9434d63ebc5c4f0e5358632b3
gnaneswar666 <gnaneswarreddy666@gmail.com>
2026-09-10 15:45:35 +0530

    CRUD ,Paging And Sorting ,JPA Repositories Implementation with git ignore

---
+++ OneToOneRelationShip/src/main/java/com/Gnaneswar/OneToOneRelationShip/OneToOneRelationShipApplication.java
@@ -0,0 +1,13 @@
+package com.Gnaneswar.OneToOneRelationShip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToOneRelationShipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneToOneRelationShipApplication.class, args);
+	}
+
+}
---
+++ OneToOneRelationShip/src/test/java/com/Gnaneswar/OneToOneRelationShip/OneToOneRelationShipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gnaneswar.OneToOneRelationShip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToOneRelationShipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
